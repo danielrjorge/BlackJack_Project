a228822020901ec48a80be144c851ec0c2ac01c9
Code Cadet <joaopedropacheco@enta.pt>
2021-10-30 20:13:28 +0000

    adding multithreading to betting

--- blACk_jACk_Game/src/Bet.java
+++ blACk_jACk_Game/src/Bet.java
@@ -1,11 +1,28 @@
 public class Bet implements Runnable{
-
-    public Bet() {
+    private Player player;
+    public Bet(Player player) {
+        this.player = player;
     }
 
     @Override
     public void run() {
+        System.out.println(player.getSocket());
+        int chipsBet;
 
+        player.getScanner().setMessage("Place your bet - Total chips available -> " + player.getChips());
+
+        while (true) {
+            chipsBet = player.getPrompt().getUserInput(player.getScanner());
+
+            if (chipsBet > player.getChips()) {
+                player.getScanner().setMessage("Not enough chips for this bet, try again");
+            } else {
+                break;
+            }
+        }
+
+        player.setBet(chipsBet);
+        player.setChips(player.getChips() - chipsBet);
 
 
     }
--- blACk_jACk_Game/src/Play.java
+++ blACk_jACk_Game/src/Play.java
@@ -1,4 +1,7 @@
 public class  Play implements Runnable{
+    public Play(Player player) {
+    }
+
     @Override
     public void run() {
 
